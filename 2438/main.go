0b448183ad2b96c2a3c4092bcdcb63ba952cc711
GS.Hyeon <qlqlqkqlqn@gmail.com>
2022-06-25 01:02:09 +0900

    add: solve 2438

---
+++ 2438/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scanf("%d", &n)
+	for i := 1; i <= n; i++ {
+		for j := 0; j < i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Print("\n")
+	}
+}
